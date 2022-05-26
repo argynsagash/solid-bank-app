dc6e33dae1923bf7f827d5c938eb2979a5954bd1
Argyn Sagash <argyn.sagash@gmail.com>
2022-05-26 18:09:39 +0600

    Create service find for transaction

--- src/main/java/kz/singularity/bankapp/features/transactions/data/repositories/TransactionRepository.java
+++ src/main/java/kz/singularity/bankapp/features/transactions/data/repositories/TransactionRepository.java
@@ -1,11 +1,14 @@
 package kz.singularity.bankapp.features.transactions.data.repositories;
 
 
+import kz.singularity.bankapp.features.accounts.data.entities.Account;
 import kz.singularity.bankapp.features.transactions.data.entities.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, String> {
-
+    List<Transaction> findAll();
 }
